a4402dc01583e88a5c415e3aae22270a3a364c7d
Alex <goketka@gmail.com>
2022-11-25 14:27:18 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+/*const result = confirm("Are you there?");
+
+const answer = +prompt("Вам есть 18?", "18");
+console.log(answer + 5);
+
+const vehicleBodyWidth = 500;
+const vehicleBodyLenght = 400;
+
+console.log("Ширина кузова машины: " + vehicleBodyWidth + ', длина: ' + vehicleBodyLenght);
+
+let number = 4.6;
+
+console.log(-4.6/0);
+console.log('string' * 9);
+
+const persone = 5;
+
+const bool = false;
+
+console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name:"John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+let arr = ['plum.png', 'orange.jpg', 6, "Alex"];
+
+const answers = [];
+
+answers[0] = prompt("Как ваше имя?", "");
+answers[1] = prompt("Как ваши дела?", "");
+answers[2] = prompt("Сколько вам лет?", "");
+
+
+const category = 'toys';
+console.log(`https://someurl.com/${category/5}`);
+
+const user = 'Alex';
+alert(`Привет, ${user}`);
+*/
+
+let incr = 10,
+    decr = 10;
+
+//incr++;
+//decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+//== - равенство
+//=== - строгое равенство
+
+const isChacked = true;
+const isClose = true;
+
+console.log(isChacked && isClose);
+console.log(isChacked || !isClose);
